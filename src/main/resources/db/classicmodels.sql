4426554153da1598b2ca84bd1266d99d9cda763a
dinobotry <dino@robotry.co.kr>
2023-08-22 22:12:19 +0900

    SQL수정
     - `customer` 테이블
        - creditLimit > credit_limit

--- src/main/resources/db/classicmodels.sql
+++ src/main/resources/db/classicmodels.sql
@@ -126,7 +126,7 @@ CREATE TABLE `customers` (
                              `postal_code` varchar(15) DEFAULT NULL,
                              `country` varchar(50) NOT NULL,
                              `sales_rep_employee_number` int(11) DEFAULT NULL,
-                             `creditLimit` decimal(10,2) DEFAULT NULL,
+                             `credit_limit` decimal(10,2) DEFAULT NULL,
                              PRIMARY KEY (`customer_number`),
                              KEY `sales_rep_employee_number` (`sales_rep_employee_number`),
                              CONSTRAINT `customers_ibfk_1` FOREIGN KEY (`sales_rep_employee_number`) REFERENCES `employees` (employee_number)
